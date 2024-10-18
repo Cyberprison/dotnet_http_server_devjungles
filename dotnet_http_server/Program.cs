b4ee5632eb9116f805706022854ce9790adbef0d
whoami <az.khabibullin2018@gmail.com>
2024-10-18 13:16:30 +0500

    init clear prjct

---
+++ dotnet_http_server/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace dotnet_http_server 
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("hiiiii");
+        }
+    }
+}
